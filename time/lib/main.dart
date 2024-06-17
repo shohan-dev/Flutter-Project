2297363a26a508eb180ecb979420ebca69be2180
Md Sabbir Roshid Shohan <sabbirshohan80@gmail.com>
2024-06-18 04:21:32 +0600

    date added

--- time/lib/main.dart
+++ time/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -20,8 +21,42 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late String _dateTimeString;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _dateTimeString = _formatDateTime(DateTime.now());
+    _timer = Timer.periodic(
+        const Duration(seconds: 1), (Timer t) => _updateDateTime());
+  }
+
+  void _updateDateTime() {
+    setState(() {
+      _dateTimeString = _formatDateTime(DateTime.now());
+    });
+  }
+
+  String _formatDateTime(DateTime dateTime) {
+    return "${dateTime.toLocal()}".split(' ')[0] +
+        " " +
+        dateTime.toLocal().toString().split(' ')[1].split('.')[0];
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +66,7 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Center(
         child: Text(
-          "${DateTime.now()}",
+          _dateTimeString,
           style: const TextStyle(fontSize: 24),
         ),
       ),
