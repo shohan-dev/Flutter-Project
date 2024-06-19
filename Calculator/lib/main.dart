0d967fcadb6600e2b80f830a3647759810bc18bf
Md Sabbir Roshid Shohan <sabbirshohan80@gmail.com>
2024-06-19 07:35:18 +0600

    Calculator

--- Calculator/lib/main.dart
+++ Calculator/lib/main.dart
@@ -8,6 +8,7 @@ class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Calculator',
       theme: ThemeData(
         primarySwatch: Colors.blue,
