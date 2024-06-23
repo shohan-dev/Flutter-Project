b80e5804d2ab4ebcce3437400ceac9c74eb44369
Md Sabbir Roshid Shohan <sabbirshohan80@gmail.com>
2024-06-24 00:25:20 +0600

    Container Animation

---
+++ Animation/lib/Screen/ImplicitAnimationsScreen/Container_Animation.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('AnimatedContainer Example'),
+        ),
+        body: ContainerDemo(),
+      ),
+    );
+  }
+}
+
+class ContainerDemo extends StatefulWidget {
+  @override
+  _ContainerDemoState createState() => _ContainerDemoState();
+}
+
+class _ContainerDemoState extends State<ContainerDemo> {
+  bool _isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            _isExpanded = !_isExpanded;
+          });
+        },
+        child: AnimatedContainer(
+          duration: Duration(seconds: 1),
+          width: _isExpanded ? 300.0 : 200.0,
+          height: _isExpanded ? 300.0 : 200.0,
+          color: _isExpanded ? Colors.red : Colors.blue,
+          child: Center(
+            child: Text(
+              'Tap to Expand',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
